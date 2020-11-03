064c72af6a75a6b5029ff83692a3c614faf80d53
Zachary Hampton <ZHampton7131@dysartstudents.org>
2020-11-03 16:51:53 +0000

    added exception handling and tie detection, fixed letter override detection

--- main.py
+++ main.py
@@ -30,8 +30,37 @@ template = f"""-------------
 
 print(template)
 
+def checkForDupe(input):
+	if input in letters:
+		return True
+	else:
+		return False
 
-def check():
+
+def checkForError(input):
+	try:
+		if int(input) < 1 or int(input) > 9:
+			return "OOR"
+	except (ValueError, TypeError):
+		return "NINT"
+
+	else:
+		return "OK"
+
+
+def check(): # function will loop through each indice of the possible winning combinations, and compare them to my game table. If one of the winning combinations has 3 of the same letter in the list, then it is classified as a win. If any of the indices in an array are not its respectable 3 numbers, iterate a counter. If all the spots are taken, they are not numbers, and will be different, therefore ticking our counter. Since the table is 9 slots, once the counter is equal to 9, the round is a tie. 
+		tttc = 0
+		for (a, b, c) in zip(ttt[0], ttt[1], ttt[2]):
+			if a not in ["1", "2", "3"]:
+				tttc += 1
+			if b not in ["4", "5", "6"]:
+				tttc += 1
+			if c not in ["7", "8", "9"]:
+				tttc += 1
+
+		if tttc == 9:
+			return "Tie"
+
 		for indivwin in winningpositions:  # For each possible win take the index of 
 			curriter = []
 			for eachindex in indivwin:  # For each number in each possible run, append that index of the TTT table into a list.
@@ -43,11 +72,27 @@ def check():
 					curriter.append(ttt[2][eachindex - 6])
 			if curriter == [letters[lettersCounter], letters[lettersCounter], letters[lettersCounter]]:
 				return True
-#				raise SystemExit
 
-while won == False:
+while won != True:
 
 	askForPlacement = input(f"{letters[lettersCounter]}, where would you like to place your {letters[lettersCounter]}? ")
+
+	checkForErrorReturn = checkForError(askForPlacement)
+	if checkForErrorReturn == "OOR":
+		while True:
+			print("Your number is out of range. Please choose a number from 1 to 9.")
+			askForPlacement = input(f"{letters[lettersCounter]}, where would you like to place your {letters[lettersCounter]}? ")
+			checkForErrorReturn = checkForError(askForPlacement)
+			if checkForErrorReturn == "OK":
+				break
+	if checkForErrorReturn == "NINT":
+		while True:
+			print("Your number is not an integer. Please try again.")
+			askForPlacement = input(f"{letters[lettersCounter]}, where would you like to place your {letters[lettersCounter]}? ")
+			checkForErrorReturn = checkForError(askForPlacement)
+			if checkForErrorReturn == "OK":
+				break
+		
 
 	if int(askForPlacement) in [1, 2, 3]:
 		inde = 0
@@ -59,19 +104,38 @@ while won == False:
 		inde = 2
 		sub = 7
 
+	isDupe = checkForDupe(ttt[inde][int(askForPlacement) - sub])
 
-
-	if ttt[inde][int(askForPlacement) - sub] in ["X", "Y"]:
-		print("Spot has already been chosen! ")
-		askForPlacement = input(f"{letters[lettersCounter]}, where would you like to place your {letters[lettersCounter]}? ")
-	else:
-		ttt[inde][int(askForPlacement) - sub] = letters[lettersCounter]
+	if isDupe == True:
+		while True:
+			print("Spot has already been chosen! ")
+			askForPlacement = input(f"{letters[lettersCounter]}, where would you like to place your {letters[lettersCounter]}? ")
+			if int(askForPlacement) in [1, 2, 3]:
+				inde = 0
+				sub = 1
+			if int(askForPlacement) in [4, 5, 6]:
+				inde = 1
+				sub = 4
+			if int(askForPlacement) in [7, 8, 9]:
+				inde = 2
+				sub = 7
+			isDupe = checkForDupe(ttt[inde][int(askForPlacement) - sub])
+			if isDupe == False:
+				break
+	if isDupe == False:
+		ttt[inde][int(askForPlacement) - sub] = letters[lettersCounter] # Sets player's letter in chosen position
 		os.system('clear')
 
+
 	checkForWin = check()
-	if checkForWin == True:
+
+
+	if checkForWin == True or checkForWin == "Tie":
 		won = True
-		print(f"{letters[lettersCounter]}, you won!")
+		if checkForWin == True:
+			print(f"{letters[lettersCounter]}, you won!")
+		if checkForWin == "Tie":
+			print("The game is a tie!")
 		time.sleep(1)
 
 		askPlayAgain = input("Would you like to play again? ")
